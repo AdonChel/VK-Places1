e475d98309e0a7c75c89106e4f93349cc4dc7bc2
AdonChel <coffi1997@inbox.ru>
2023-12-18 21:48:31 +0500

    Three commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import { View, Panel, PanelHeader, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol, Group, Button, Separator, Spacing, Title, IconButton, Subhead, Image, Epic, Tabbar, TabbarItem, FormLayout, FormItem, Input, FormField, Checkbox, Search } from '@vkontakte/vkui';
+import { View, Panel, PanelHeader, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol, Group, Button, Separator, Spacing, Title, IconButton, Subhead, Image, Epic, Tabbar, TabbarItem, FormLayout, FormItem, Input, FormField, Checkbox, Search, ModalPage, ModalPageHeader, PanelHeaderClose, PanelHeaderSubmit, ModalRoot } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import '@vkontakte/icons'
 import './styles/background.css'
@@ -13,6 +13,7 @@ import {Icon28AchievementCircleFillBlue} from '@vkontakte/icons'
 import {Icon28UserSquareOutline} from '@vkontakte/icons'
 import {Icon28LocationMapOutline} from '@vkontakte/icons'
 import {Icon28LogoVk} from '@vkontakte/icons'
+import { getSizeX } from '@vkontakte/vkui/dist/lib/adaptivity';
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('start-screen');
@@ -172,19 +173,97 @@ const App = () => {
 								<Panel id='start-6'>
 									<Group separator='hide'>
 										<div style={containerStyles} className='div-screen5'>
+										
 											<div className='div-container'>
-												<Search value={Search} className='search'>
+												<Search type='text' placeholder='Поиск мест и адресов' className='search'>
 												
 												</Search>
 											</div>			
 											<Tabbar>
-												<TabbarItem text='Профиль'>
+												<TabbarItem onClick={() => setActivePanel('profile')} text='Профиль'>
 													<Icon28UserSquareOutline/>
 												</TabbarItem>
-												<TabbarItem text='Места'>
+												<TabbarItem onClick={() => setActivePanel('places')} text='Места'>
 													<Icon28LogoVk />
 												</TabbarItem>
-												<TabbarItem text='Рекомендации'>
+												<TabbarItem onClick={() => setActivePanel('recomendation')} text='Рекомендации'>
+													<Icon28LocationMapOutline />
+												</TabbarItem>
+											</Tabbar>
+										</div>
+									</Group>
+								</Panel>
+								<Panel id='profile'>
+									<Group separator='hide'>
+										<div style={containerStyles} className='div-screen6'>
+											<div className='div-container'>
+											<div className='window4'>
+													<Spacing size={110}></Spacing>
+												<Title style={{marginLeft: '0vh', width: '58vh', fontSize: '20px'}} className='text1'>Продвинутая персона рекомендует!</Title>
+												<div className='white5'>
+													
+												</div>
+												<Title className='text1' style={{marginLeft: '13vh'}}>"Кировка"</Title>
+												<Spacing size={10}></Spacing>
+												<div className='white6'>
+												<Title className='text1' style={{fontSize: '16px', padding: '11px'}}>Это самая популярная улица города Челябинска — приходите в будни, чтобы не пришлось убирать людей в фотошопе. Лучшие кадры получатся, если выбрать ракурс в сторону театра оперы и балета или особняка-ресторана «Цыплята табака». Кстати, это первое заведение города</Title>
+												</div>
+											</div>	
+											</div>
+											<Tabbar>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Профиль'>
+													<Icon28UserSquareOutline/>
+												</TabbarItem>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Места'>
+													<Icon28LogoVk />
+												</TabbarItem>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Рекомендации'>
+													<Icon28LocationMapOutline />
+												</TabbarItem>
+											</Tabbar>
+										</div>
+									</Group>
+								</Panel>
+								<Panel id='places'>
+									<Group separator='hide'>
+										<div style={containerStyles} className='div-screen6'>
+											<div className='div-container'>
+												<Spacing size={70}></Spacing>
+												<div className='window5'>
+												</div>
+											</div>
+											<Tabbar>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Профиль'>
+													<Icon28UserSquareOutline/>
+												</TabbarItem>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Места'>
+													<Icon28LogoVk />
+												</TabbarItem>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Рекомендации'>
+													<Icon28LocationMapOutline />
+												</TabbarItem>
+											</Tabbar>
+										</div>
+									</Group>
+								</Panel>
+								<Panel id='recomendation'>
+									<Group separator='hide'>
+										<div style={containerStyles} className='div-screen6'>
+											<div className='div-container1'>
+												<Search type='text' placeholder='Поиск мест и адресов' className='search'>
+												</Search>
+												
+											</div>
+											<div className='window6'>
+												</div>
+											<Tabbar>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Профиль'>
+													<Icon28UserSquareOutline/>
+												</TabbarItem>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Места'>
+													<Icon28LogoVk />
+												</TabbarItem>
+												<TabbarItem onClick={() => setActivePanel('start-6')} text='Рекомендации'>
 													<Icon28LocationMapOutline />
 												</TabbarItem>
 											</Tabbar>
